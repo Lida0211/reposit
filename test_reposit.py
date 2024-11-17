ba2841929e267b9edf4b09f07cf5e13f64480d7f
Lida0211 <lapanoviclidia@gmail.com>
2024-11-17 15:03:09 +0300

    Add files via upload

---
+++ test_reposit.py
@@ -0,0 +1,15 @@
+def func(x,y):
+    return x + y
+
+def division(x,y):
+    return x // y
+
+
+
+print(func(1,2))
+print(division(1,2))
+def func2(x,y):
+    return x * y
+
+print(func(1,2))
+print(func2(1,2))
